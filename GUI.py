5620c1545e7271fca7dba78d10742cc835ee0055
Gabriel <g.abreu@aluno.ufabc.edu.br>
2023-01-28 13:55:10 -0300

    initial commit

---
+++ GUI.py
@@ -0,0 +1,10 @@
+from tkcalendar import Calendar, DateEntry
+c0 = "#D1D1D1"
+c1 = "#DBDBDB"
+c2 = "#85C7F2"
+c3 = "#636363"
+c4 = "#4C4C4C"
+
+        self.entry_data = DateEntry(self.form)
+        self.entry_data.configure(width=12)
+ 
